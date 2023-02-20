e0db39166732b883a96fbb6399ddee3b06efe98c
SamuelGoncalves7 <124736734+SamuelGoncalves7@users.noreply.github.com>
2023-02-20 18:55:01 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+function functionToExecute(){
+    window.location.assign("http://www.youtube.com");
+    window.location.assign("file:///Users/samuelgoncalves/Documents/PROJECTS/Staccato-Learn/beginner.html")
+}
+
+/*
+function functionToExecute(page) {
+    let page = String(page);
+    page = page.concat(".html");
+    let baseurl = "file:///Users/samuelgoncalves/Documents/PROJECTS/Staccato-Learn/";
+    let finurl = baseurl.concat(page);
+    window.location.assign(finurl);
+  }*/
+
+
